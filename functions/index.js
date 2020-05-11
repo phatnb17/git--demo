a4b17f83e7b91337c3c0d92f6e759f0230c07b94
unknown <nguyenphatpassion@gmail.com>
2020-05-12 00:22:42 +0700

    video2

---
+++ functions/index.js
@@ -0,0 +1,54 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const serviceAccount = require("./key/localhost.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const express = require("express");
+const app = express();
+
+app.get("/screams", (req, res) => {
+  admin
+    .firestone()
+    .collection("screams")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.post("/scream", (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString(),
+  };
+
+  admin
+    .firestone()
+    .collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+
+
+exports.api = functions.https.onRequest(app);
