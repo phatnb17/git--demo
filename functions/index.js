4aadfb96b0d26e2227cd8f9538b7d563bf1fe096
unknown <nguyenphatpassion@gmail.com>
2020-05-18 11:54:15 +0700

    #5 validation & login

--- functions/index.js
+++ functions/index.js
@@ -7,26 +7,23 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-const  config ={
- 
-    apiKey: "AIzaSyAGM-bkY2-EO4V7lAtREp5I9jUDSfSc39g",
-    authDomain: "tutorial-267c7.firebaseapp.com",
-    databaseURL: "https://tutorial-267c7.firebaseio.com",
-    projectId: "tutorial-267c7",
-    storageBucket: "tutorial-267c7.appspot.com",
-    messagingSenderId: "29446722940",
-    appId: "1:29446722940:web:fc543c1aef36ef285340f9",
-    measurementId: "G-QJ3FSDJPWK"
-  
+const config = {
+  apiKey: "AIzaSyAGM-bkY2-EO4V7lAtREp5I9jUDSfSc39g",
+  authDomain: "tutorial-267c7.firebaseapp.com",
+  databaseURL: "https://tutorial-267c7.firebaseio.com",
+  projectId: "tutorial-267c7",
+  storageBucket: "tutorial-267c7.appspot.com",
+  messagingSenderId: "29446722940",
+  appId: "1:29446722940:web:fc543c1aef36ef285340f9",
+  measurementId: "G-QJ3FSDJPWK",
 };
 
-const firebase = require('firebase');
+const firebase = require("firebase");
 firebase.initializeApp(config);
 const db = admin.firestore();
 
 app.get("/screams", (req, res) => {
-  db
-    .collection("screams")
+  db.collection("screams")
     .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
@@ -48,11 +45,10 @@ app.post("/scream", (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: new Date().toISOString()
+    createdAt: new Date().toISOString(),
   };
 
-  db
-    .collection("screams")
+  db.collection("screams")
     .add(newScream)
     .then((doc) => {
       res.json({ message: `document ${doc.id}  created successfully` });
@@ -62,51 +58,105 @@ app.post("/scream", (req, res) => {
       console.error(err);
     });
 });
+const isEmpty = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
 // signup
-app.post('/signup',(req,res) => {
-  const newUser ={
+app.post("/signup", (req, res) => {
+  const newUser = {
     email: req.body.email,
     password: req.body.password,
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle,
   };
+  let errors = {};
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty!";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address!";
+  }
+
+  if (isEmpty(newUser.password)) errors.password = "Must not be empty!";
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = "PassWords must match";
+  if (isEmpty(newUser.handle)) errors.handle = "Must not be empty";
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
   //todo
+
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
-  .get()
-  .then((doc) =>{
-    if(doc.exists){
-      return res.status(400).json({handle: 'this handle is already taken'});
-    }else{
-     return firebase
-      .auth()
-      .createUserWithEmailAndPassword(newUser.email, newUser.password);
-    }
-  })
-  .then((data)=>{
-    userId = data.user.uid;
-    return data.user.getIdToken();
-  })
-  .then((idToken)=>{
-    token = idToken;
-    const userCredentials ={
-      handle: newUser.handle,
-      email: newUser.email,
-      createdAt: new Date().toISOString(),
-      userId
-    };
-    return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-  })
-  .then(()=>{
-    return res.status(201).json({ token });
-  })
-  .catch(err =>{
-    console.error(err);
-    if (err.code === 'auth/email-already-in-use'){
-      return res.status(400).json({ email:'Email is already is use'});
-    }else{
-      return res.status(500).json({ error: err.code});
-    }
-  });
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already is use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
 });
-exports.api = functions.region('asia-northeast1').https.onRequest(app);
+
+app.post("/login", (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+  let errors = {};
+
+  if (isEmpty(user.email)) errors.email = "Must not be empty email!";
+  if (isEmpty(user.password)) errors.password = "Must not be empty password!";
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/wrong-password") {
+        return res
+          .status(403)
+          .json({ general: "Wrong credentials, please try again." });
+      } else return res.status(500).json({ error: err.code });
+    });
+});
+exports.api = functions.region("asia-northeast1").https.onRequest(app);
