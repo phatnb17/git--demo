5dfb4b43c8631a692feb35761483da2df11f25c0
unknown <nguyenphatpassion@gmail.com>
2020-05-18 14:49:18 +0700

    #6 Authorization

--- functions/index.js
+++ functions/index.js
@@ -41,11 +41,48 @@ app.get("/screams", (req, res) => {
     .catch((err) => console.error(err));
 });
 
-app.post("/scream", (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer ')
+  ) {
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: 'Unauthorized'});
+  }
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodeToken) => {
+      req.user = decodeToken;
+      console.log(decodeToken);
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then((data) => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch((err) => {
+      console.error("error while verifying token", err);
+      return res.status(403).json(err);
+    });
+};
+//Post
+
+app.post("/scream", FBAuth, (req, res) => {
+  if (req.body.body.trim() === "") {
+    return res.status(400).json({ body: "body must not be empty" });
+  }
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
-    createdAt: new Date().toISOString(),
+    userHandle: req.user.handle,
+    createdAt: new Date().toISOString()
   };
 
   db.collection("screams")
@@ -58,15 +95,18 @@ app.post("/scream", (req, res) => {
       console.error(err);
     });
 });
-const isEmpty = (string) => {
-  if (string.trim() === "") return true;
-  else return false;
-};
+
 const isEmail = (email) => {
   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (email.match(regEx)) return true;
   else return false;
 };
+
+const isEmpty = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
 // signup
 app.post("/signup", (req, res) => {
   const newUser = {
